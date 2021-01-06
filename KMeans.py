c6003c7ab3d887deb87544869953447fc58cbcb9
JerryJack121 <73692359+JerryJack121@users.noreply.github.com>
2021-01-06 18:09:20 +0800

    Update

--- KMeans.py
+++ KMeans.py
@@ -13,12 +13,12 @@ kmeans_fit = cluster.KMeans(n_clusters = 3).fit(X)
 
 # 印出分群結果
 cluster_labels = kmeans_fit.labels_
-# print("分群結果：")
-# print(cluster_labels)
-# print("---")
+kmeans_centers = kmeans_fit.cluster_centers_
+print('kmeans_centers:\n', kmeans_centers)
 
 # plot
 fig = plt.figure()
 ax = Axes3D(fig)
 ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=cluster_labels, cmap='Set1')
+ax.scatter(kmeans_centers[:, 0], kmeans_centers[:, 1], kmeans_centers[:, 2], c='b', marker=(5, 1), cmap='Set1', s=100)
 plt.show()
--- fcm.py
+++ fcm.py
@@ -4,7 +4,6 @@ from matplotlib import pyplot as plt
 import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 
-n_samples = 3000
 
 df = pd.read_excel('120_data.xlsx', header=None)
 X = df.to_numpy()
@@ -16,9 +15,11 @@ fcm.fit(X)
 # outputs
 fcm_centers = fcm.centers
 fcm_labels = fcm.predict(X)
+print('fcm_centers:\n', fcm_centers)
 
 # plot result
 fig = plt.figure()
 ax = Axes3D(fig)
 ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=fcm_labels, cmap='Set1')
+ax.scatter(fcm_centers[:, 0], fcm_centers[:, 1], fcm_centers[:, 2], c='b', marker=(5, 1), s=100)
 plt.show()
